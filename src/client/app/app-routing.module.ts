d540ea64912534be25d95d10e0d67f513b992394
pigunther <pigunther16@gmail.com>
2018-03-07 15:36:45 +0300

    route changed

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {TabsExampleComponent} from "./tabs-example/tabs-example.component";
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
+      {
+        path: 'MyTabView',
+        component: TabsExampleComponent,
+        data: { title: 'Heroes List' }
+      },
+      {
+        path: '',
+        redirectTo: '/MyTabView',
+        pathMatch: 'full'
+      }
       /* define app module routes here, e.g., to lazily load a module
          (do not place feature module routes here, use an own -routing.module.ts in the feature instead)
        */
--- src/client/app/tabs-example/tabs-example-routing.module.ts
+++ src/client/app/tabs-example/tabs-example-routing.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {MyTabView} from "../tab/tab-view";
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {TabsExampleComponent} from "./tabs-example.component";
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'myTabView', component: TabsExampleComponent}
+      {path: 'myTabView', component: TabsExampleComponent}
     ])
   ],
   exports: [RouterModule]
 })
-export class TabsExampleRoutingModule { }
+export class TabsExampleRoutingModule {
+}

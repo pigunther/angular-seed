bc62525f63fc534e199709da4ebfa95cd2536c9c
pigunther <pigunther16@gmail.com>
2018-03-07 15:44:18 +0300

    ng-template added

---
+++ src/client/app/tab/tab-panel.component.ts
@@ -0,0 +1,51 @@
+import {
+  AfterContentChecked,
+  AfterContentInit, AfterViewInit,
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewEncapsulation
+} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  encapsulation: ViewEncapsulation.None,
+  selector: 'my-tab-panel',
+  templateUrl: 'tab-panel.component.html',
+  styleUrls: ['tab-panel.component.css']
+})
+export class MyTabPanel implements OnInit, AfterContentInit, AfterViewInit {
+
+  @Input() header: string;
+
+  @Input() selected: boolean = false;
+  @Input() disabled: boolean = false;
+  @Input() closable: boolean = false;
+
+  @ContentChild(TemplateRef) customContent: TemplateRef<any>;
+
+  innerTemplate: TemplateRef<any>;
+
+  closed: boolean;
+
+
+  ngOnInit() {
+    console.log('tab ' + this.header + ' was initialized with selected ' + this.selected);
+  }
+
+  ngAfterContentInit() {
+    this.innerTemplate = this.customContent;
+    console.log('tab ' + this.header + ' content was initialized with selected ' + this.selected);
+  }
+
+  ngAfterViewInit() {
+    this.innerTemplate = this.customContent;
+    console.log('tab ' + this.header + ' view was initialized');
+  }
+  // ngAfterContentChecked() {
+  //   console.log('tab ' + this.header + ' checked content ');
+  // }
+}

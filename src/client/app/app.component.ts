25237a1a63ca690e4d4ee9d82a112b915b4d1e64
pigunther <pigunther16@gmail.com>
2018-03-16 00:56:56 +0300

    minor style changes, add position:sticky

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import { Config } from './shared/config/env.config';
 import './operators';
 
@@ -8,6 +8,7 @@ import './operators';
 @Component({
   moduleId: module.id,
   selector: 'sd-app',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css', 'normalize.css'],
 })
